666c4b38ec2af15ba7750e030f0950579b3a494f
Sebastian Pukki <sebastian.pukki@aptiv.com>
2019-03-07 13:22:48 +0100

    Adds simple graph to Android app with sample data

--- app-android/app/src/main/java/com/aptiv/watchdogapp/MainActivity.kt
+++ app-android/app/src/main/java/com/aptiv/watchdogapp/MainActivity.kt
@@ -2,11 +2,33 @@ package com.aptiv.watchdogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import com.jjoe64.graphview.GraphView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addDummyData()
+    }
+
+    // http://www.android-graphview.org/simple-graph/
+    private fun addDummyData() {
+        val graph = findViewById<GraphView>(R.id.graph)
+
+        val series = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+
+        graph.addSeries(series)
     }
 }
