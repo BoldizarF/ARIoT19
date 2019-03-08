3041d5aeeac960f06288ec48a0ed726ed42b9a82
Alex Sackey <lexious89@yahoo.com>
2019-03-08 21:12:41 +0100

    additional required header files

---
+++ wristwatch/sensors.h
@@ -0,0 +1,13 @@
+/**
+Header file for all the sensprs in the wearable device #2.
+
+
+
+
+
+*/
+
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.  
+
+#include <DallasTemperature.h>
