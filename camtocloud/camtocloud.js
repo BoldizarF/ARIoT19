ead1735ff66a2bd5f23275d43af267c991bd7441
ddjohn <ddjohn@gmail.com>
2019-03-08 08:20:06 +0100

    Update camtocloud.js
    Skipping several frames to make the picture better

--- camtocloud/camtocloud.js
+++ camtocloud/camtocloud.js
@@ -5,32 +5,33 @@ var fs = require('fs');
 var http = require('http');
 
 var cloud = {
-        host:   'ariot-env.npn96gumht.us-east-2.elasticbeanstalk.com',
-        port:   80,
-        path:   '/api/v1/images?apikey=666',
-        method: 'POST',
-        headers: {
-                'Content-Type': 'application/json',
-        },
+	host:   'ariot-env.npn96gumht.us-east-2.elasticbeanstalk.com',
+	port:   80,
+	path:   '/api/v1/images?apikey=666',
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
 };
 
 var webcam = NodeWebCam.create({
-        width:   400,
-        height:  300,
-        quality: 50,
-        delay:   0,
-        skip:    20,
+	width:   352,
+	height:  288,
+	quality: 100,
+	delay:   0,
+	skip:    20,
+	verbose: false,
 });
 
-webcam.capture('temp', (err,data) => {
-        if(err === null) {
-                var request = http.request(cloud, (res) => {
-                        console.log(res.statusMessage);
-                });
-                request.write(JSON.stringify(fs.readFileSync(data).toString('base64')));
-                request.end();
-        } else {
-                console.log("Error:");
-                console.log(err);
-        }
+webcam.capture('temp', (err, data) => {
+	if(err === null) {
+		var request = http.request(cloud, (res) => {
+			console.log(res.statusMessage);	
+		});
+		request.write(JSON.stringify(fs.readFileSync(data).toString('base64')));
+		request.end();
+	} else {
+		console.log("Error:");
+		console.log(err);
+	}
 });
