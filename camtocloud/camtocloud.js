fb0c3b20f57b99e7b9dec411cca706836b20e97e
ddjohn <ddjohn@gmail.com>
2019-03-08 09:15:37 +0100

    Camera triggered by GPIO pin

--- camtocloud/camtocloud.js
+++ camtocloud/camtocloud.js
@@ -3,6 +3,9 @@
 var NodeWebCam = require('node-webcam');
 var fs = require('fs');
 var http = require('http');
+var Gpio = require('onoff').Gpio;
+var led = new Gpio(17, 'out');
+var input = new Gpio(27, 'in', 'falling');
 
 var cloud = {
 	host:   'ariot-env.npn96gumht.us-east-2.elasticbeanstalk.com',
@@ -23,15 +26,39 @@ var webcam = NodeWebCam.create({
 	verbose: false,
 });
 
-webcam.capture('temp', (err, data) => {
+input.watch( (err, value) => {
+	console.log("Got input...");
 	if(err === null) {
-		var request = http.request(cloud, (res) => {
-			console.log(res.statusMessage);	
+
+		led.writeSync(1);
+		webcam.capture('temp', (err, data) => {
+			if(err === null) {
+				var request = http.request(cloud, (res) => {
+					console.log(res.statusMessage);	
+				});
+				request.write(JSON.stringify(fs.readFileSync(data).toString('base64')));
+				request.end();
+
+				led.writeSync(0);
+			} else {
+				console.log("Error:");
+				console.log(err);
+
+				led.writeSync(0);
+			}
 		});
-		request.write(JSON.stringify(fs.readFileSync(data).toString('base64')));
-		request.end();
-	} else {
-		console.log("Error:");
-		console.log(err);
+	}
+	else {
+				console.log("Error:");
+				console.log(err);
 	}
 });
+
+function unexportOnExit() {
+	console.log("Exiting...");
+	input.unexport();
+	led.unexport();
+}
+
+process.on('SIGINT', unexportOnExit);
+console.log("Started...");
